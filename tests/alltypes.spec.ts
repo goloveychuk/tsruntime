ee81b776783d03bc9a439ee8ee86c9d98f1745dc
Vadym <goloveychuk1@gmail.com>
2017-05-11 13:57:19 +0300

    travis

--- tests/alltypes.spec.ts
+++ tests/alltypes.spec.ts
@@ -23,10 +23,10 @@ class AllTypes {
     null: null
     void: void
     never: never
-    union: string | number
     symbol: symbol
     array: string[]
     // object: {}
+    union: string | number
     tuple: [string, number]
 
     enum: Enum
@@ -69,9 +69,37 @@ class DerrivedTypes {
 
 
 describe('All types', () => {
-   it('number', () => {
+   it('string', () => {
       const clsType = getPropType(AllTypes, "string") as Types.StringType;
       expect(clsType.kind).toBe(Types.TypeKind.String)
+   });
+   it('number', () => {
+      const clsType = getPropType(AllTypes, "number") as Types.NumberType;
+      expect(clsType.kind).toBe(Types.TypeKind.Number)
+   });
+   it('any', () => {
+      const clsType = getPropType(AllTypes, "any") as Types.AnyType;
+      expect(clsType.kind).toBe(Types.TypeKind.Any)
+   });
+   it('undefined', () => {
+      const clsType = getPropType(AllTypes, "undefined") as Types.UndefinedType;
+      expect(clsType.kind).toBe(Types.TypeKind.Undefined)
+   });
+    it('null', () => {
+      const clsType = getPropType(AllTypes, "null") as Types.NullType;
+      expect(clsType.kind).toBe(Types.TypeKind.Null)
+   });
+   it('void', () => {
+      const clsType = getPropType(AllTypes, "void") as Types.VoidType;
+      expect(clsType.kind).toBe(Types.TypeKind.Void)
+   });
+   it('never', () => {
+      const clsType = getPropType(AllTypes, "never") as Types.NeverType;
+      expect(clsType.kind).toBe(Types.TypeKind.Never)
+   });
+   it('symbol', () => {
+      const clsType = getPropType(AllTypes, "symbol") as Types.ESSymbolType;
+      expect(clsType.kind).toBe(Types.TypeKind.ESSymbol)
    });
 });
 
