ff8e52e4d940b56c8b9c46585de498ac9cdc6c3c
Vadym Holoveichuk <goloveychuk1@gmail.com>
2019-04-22 17:19:56 +0300

    tests

--- packages/tests/src/types/objectTypes.test.ts
+++ packages/tests/src/types/objectTypes.test.ts
@@ -11,15 +11,9 @@ const stringType = {kind: Types.TypeKind.String}
 type ObjectType = {
     key: string
     42: string
-    [uniqSymb]: string
+    // [uniqSymb]: string
 }
 
-
-
-type symbTypeAlias = typeof uniqSymb
-
-
-type StrOrNull<T> = {[K in keyof T]: string | null}
 
 
 describe("object types", () => {
@@ -31,18 +25,7 @@ describe("object types", () => {
         properties: {
             'key': stringType,
             42: stringType,
-            [uniqSymb]: stringType
-        }
-    })
-  })
-  it('mapped - record', () => {
-      const type = reflect<Record<symbTypeAlias | 'key' | 42, string>>()
-      expect(type).toEqual({
-        kind: Types.TypeKind.Object,
-        properties: {
-            'key': stringType,
-            42: stringType,
-            [uniqSymb]: stringType
+            // [uniqSymb]: stringType
         }
     })
   })
