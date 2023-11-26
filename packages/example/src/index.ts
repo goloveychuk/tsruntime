c38b585026cd2e3a4eda026e57ed8da102314669
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2023-11-26 00:01:06 -0300

    add prop type example

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -4,17 +4,30 @@ import {reflect, Reflective, getClassType} from 'tsruntime'
 
 
 
+interface Address {
+    city?: string
+    street: string
+}
 
 
 
 @Reflective
 class Cls {
-    str!: string
+    public str!: string
+    protected num?: number
+    private names!: Array<string>
+    address!: Address
+    status: 'requirer' | 'city' | 'accept' | 'reject' = 'accept';
+
+    constructor(public foo: string, private bar: number) {
+        console.log(foo, bar)
+    }
 }
 
 
 
-const type = reflect<string>()
+const type = reflect<Cls>()
+const classType = getClassType(Cls);
 
-console.log(getClassType(Cls))
+console.log(classType)
 console.log(type)
