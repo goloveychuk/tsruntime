ba51be225ae867e6bb12acd79a6b04509237e1ad
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2023-12-11 21:09:54 -0300

    Import/Export ModifierFlags from typescript

--- packages/tsruntime/src/runtime/publicTypes.ts
+++ packages/tsruntime/src/runtime/publicTypes.ts
@@ -1,3 +1,7 @@
+import { ModifierFlags } from "typescript";
+
+export { ModifierFlags } from "typescript";
+
 export enum TypeKind {
   Any = 1,
   String,
@@ -25,28 +29,6 @@ export enum TypeKind {
   Function,
 
   Unknown2 = 999
-}
-
-export enum ModifierFlags {
-  None = 0,
-  Export = 1,
-  Ambient = 2,
-  Public = 4,
-  Private = 8,
-  Protected = 16,
-  Static = 32,
-  Readonly = 64,
-  Abstract = 128,
-  Async = 256,
-  Default = 512,
-  Const = 2048,
-  HasComputedFlags = 536870912,
-  AccessibilityModifier = 28,
-  ParameterPropertyModifier = 92,
-  NonPublicAccessibilityModifier = 24,
-  TypeScriptModifier = 2270,
-  ExportDefault = 513,
-  All = 3071
 }
 
 export type StringType = BaseType<TypeKind.String, string>;
@@ -78,7 +60,7 @@ export type SimpleTypes =
   | UnknownType
   | Unknown2Type;
 
-  
+
 
 export type ReflectedType =
   | ObjectType
