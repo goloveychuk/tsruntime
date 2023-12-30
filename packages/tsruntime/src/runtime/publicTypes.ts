f37f882b3abad938eddf945079ca0725d8bc37f3
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2023-12-30 15:39:26 -0300

    Remove ModifierFlags import from typescript

--- packages/tsruntime/src/runtime/publicTypes.ts
+++ packages/tsruntime/src/runtime/publicTypes.ts
@@ -1,6 +1,24 @@
-import { ModifierFlags } from "typescript";
-
-export { ModifierFlags } from "typescript";
+export enum ModifierFlags {
+  None = 0,
+  Export = 1,
+  Ambient = 2,
+  Public = 4,
+  Private = 8,
+  Protected = 16,
+  Static = 32,
+  Readonly = 64,
+  Abstract = 128,
+  Async = 256,
+  Default = 512,
+  Const = 2048,
+  HasComputedFlags = 536870912,
+  AccessibilityModifier = 28,
+  ParameterPropertyModifier = 92,
+  NonPublicAccessibilityModifier = 24,
+  TypeScriptModifier = 2270,
+  ExportDefault = 513,
+  All = 3071
+}
 
 export enum TypeKind {
   Any = 1,
